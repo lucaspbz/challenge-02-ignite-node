f1cea55d53c4ea0b0a6aaca4b8639b4e72da5ec6
Lucas Pinheiro <lucas.pbezerra@hotmail.com>
2021-03-17 09:37:32 -0300

    final comit

--- src/index.js
+++ src/index.js
@@ -10,19 +10,68 @@ app.use(cors());
 const users = [];
 
 function checksExistsUserAccount(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const user = users.find(user => user.username === username)
+
+  if (!user) {
+    return response.status(404).json()
+  }
+  request.user = user
+  next()
 }
 
 function checksCreateTodosUserAvailability(request, response, next) {
-  // Complete aqui
+  const { user } = request
+
+  if (user.pro) {
+    return next()
+  }
+  if (user.todos.length >= 10) {
+    return response.status(403).json()
+  }
+  next()
 }
 
 function checksTodoExists(request, response, next) {
-  // Complete aqui
+  const { username } = request.headers
+  const { id } = request.params
+
+  const user = users.find(user => user.username === username)
+
+  if (!user) {
+    return response.status(404).json()
+  }
+
+  if (!validate(id)) {
+    return response.status(400).json()
+  }
+
+  const todo = user.todos.find(todo => todo.id === id)
+
+
+  if (!todo) {
+    return response.status(404).json({ error: 'No such todo with this id' })
+  }
+  request.todo = todo
+  request.user = user
+  next()
+
 }
 
 function findUserById(request, response, next) {
-  // Complete aqui
+  const { id } = request.params
+
+  if (!validate(id)) {
+    return response.status(400).json()
+  }
+
+  const user = users.find(user => user.id === id)
+
+  if (!user) {
+    return response.status(404).json()
+  }
+  request.user = user
+  next()
 }
 
 app.post('/users', (request, response) => {
